bb0bf859b1a8f5f96a9a70f0be7e5870db3fe4dd
Sharon Zacharia <zachariasharon@gmail.com>
2023-12-20 02:24:35 +0000

    created bar chart

--- infographics.py
+++ infographics.py
@@ -1,5 +1,27 @@
-import pandas as pd 
-import matplotlib.pyplot as plt 
-import seaborn as sns 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Path to original dataset
+dataset_Path = "WorldHappiness_Corruption_2015_2020.csv"
+
+#Selected countries for analysis
+COUNTRY_NAMES = [
+    "Burundi","Togo","Afghanistan","Finland","Norway","Switzerland","Luxembourg",
+    "Ireland","Saudi Arabia", "Spain","Kyrgyzstan","Ghana","Tajikistan","Senegal"
+]
 
 
+#Fucntion to read dataset
+def readDataSet(path):
+    df = pd.read_csv(path)
+    cleaned_df = df.dropna()
+    return cleaned_df
+
+
+cleaned_df = readDataSet(dataset_Path)
+countries = cleaned_df[ (cleaned_df["Country"].isin(COUNTRY_NAMES)) & (cleaned_df["Year"]==2015)]
+
+plt.bar(countries["Country"], countries["happiness_score"])
+plt.xticks(rotation=45)
+plt.show()
