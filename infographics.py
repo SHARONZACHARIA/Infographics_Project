971ba6afc37710349a84f98a3d762f55dbc4ae8d
Sharon Zacharia <zachariasharon@gmail.com>
2023-12-27 23:33:17 +0000

    plotted graphs

--- infographics.py
+++ infographics.py
@@ -9,8 +9,7 @@ dataset_Path = "WorldHappiness_Corruption_2015_2020.csv"
 
 #Selected countries for analysis
 COUNTRY_NAMES = [
-    "Burundi","Togo","Afghanistan","Finland","Norway","Switzerland","Luxembourg",
-    "Ireland","Saudi Arabia", "Spain","Kyrgyzstan","Ghana","Tajikistan","Senegal"
+    "Togo","Afghanistan","Finland","Norway","Switzerland",
     ]
 
 year = 2020
@@ -22,24 +21,57 @@ def readDataSet(path):
     cleaned_df = df.dropna()
     return cleaned_df
 
-
 cleaned_df = readDataSet(dataset_Path)
+df_filtered = cleaned_df[cleaned_df['Country'].isin(COUNTRY_NAMES)]
 
-countries_2020 =  cleaned_df[["Country","happiness_score"]][cleaned_df["Year"]==2020].head(5)
-countries_2019 =  cleaned_df[["Country","happiness_score"]][cleaned_df["Year"]==2019].head(5)
-countries_2018 =  cleaned_df[["Country","happiness_score"]][cleaned_df["Year"]==2018].head(5)
+filetred_df_Hindex = df_filtered[["Country","happiness_score","Year"]]
+pivot_df_Hindex = filetred_df_Hindex.pivot(index='Year', columns='Country', values='happiness_score')
 
-print(countries_2020)
 
-#Happiness score Bar graph for the year 2020
-def barGraph(c2020,c2019,c2018):
-    x = np.arange(5)
-    width = 0.2
-    plt.bar(x - 0.2,c2020, width, color = 'cyan')
-    plt.bar(x,c2019, width, color='green')
-    plt.bar(x + 0.2 ,c2018,width, color='orange')
-    plt.show()
-  
-barGraph(countries_2020,countries_2019,countries_2018)
+#Happiness Score Bar-graph for the years 2015 - 2020 
 
+def barGraph(pivot_df):
+	plt.figure(figsize=(10, 6))
+	pivot_df.plot(kind='bar', width=0.8,  ax=plt.gca())
+	plt.xlabel('Year')
+	plt.ylabel('Happiness Index')
+	plt.title('Happiness Index by Country for 2015 - 2020')
+	plt.legend(title='Country', bbox_to_anchor=(1.05, 1), loc='upper left')
+	plt.tight_layout()
+	plt.show()
+      
+
+df_filtered_CPI = df_filtered[["Country","cpi_score","Year"]]
+pivot_df_CPI = df_filtered_CPI.pivot(index='Year', columns='Country', values='cpi_score')
+
+#CPI score for the years 2015 - 2020
+def Hbar(pivot_df):
+	pivot_df.plot(kind="barh")
+	plt.xlabel('CPI Score')
+	plt.ylabel('Year')
+	plt.title('CPI Score by Country for 2015 - 2020')
+	plt.legend(title='Country', bbox_to_anchor=(1.05, 1), loc='upper left')
+	plt.show()
+
+
+df_filtered_dystopia = df_filtered[["Country","dystopia_residual"]]
+
+def PlotPie(pivot_df):
+	plt.pie(pivot_df["dystopia_residual"],labels = pivot_df["Country"],autopct='%1.1f%%', startangle=140)
+	plt.show()
+	
+
+df_filtered_Freedom = df_filtered[["Country","freedom","Year"]]
+pivot_df_Freedom = df_filtered_Freedom.pivot(index='Year', columns='Country', values='freedom')
+
+
+def plotLine(pivot_df):
+	pivot_df.plot(kind="line")
+	plt.show()
+
+  	
+barGraph(pivot_df_Hindex)
+Hbar(pivot_df_CPI)
+PlotPie(df_filtered_dystopia)
+plotLine(pivot_df_Freedom)
 
