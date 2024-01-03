c919e46cf5509a26a0eeafa81944ee80389bf6d2
Sharon Zacharia <zachariasharon@gmail.com>
2024-01-03 22:07:29 +0000

    before sns

--- 22074407.py
+++ 22074407.py
@@ -12,11 +12,11 @@ COUNTRY_NAMES = [
 ]
 
 ColorMap = {
-    "Togo": "#edc951",
-    "Afghanistan": "#eb6841",
-    "Finland": "#cc2a36",
-    "Norway": "#4f372d",
-    "Switzerland": "#00a0b0"
+    "Togo": "#58508d",
+    "Afghanistan": "#ff6361",
+    "Finland": "#bc5090",
+    "Norway": "#ffa600",
+    "Switzerland": "#003f5c"
 }
 
 YEARS = [2018, 2019, 2020]
@@ -28,8 +28,9 @@ def readDataSet(path):
     df = pd.read_csv(path)
     cleaned_df = df.dropna()
     return cleaned_df
-cleaned_df = readDataSet(dataset_Path)
 
+
+cleaned_df = readDataSet(dataset_Path)
 df_filtered = cleaned_df[cleaned_df['Country'].isin(COUNTRY_NAMES)]
 filetred_df_Hindex = df_filtered[["Country", "happiness_score", "Year"]]
 pivot_df_Hindex = filetred_df_Hindex.pivot(
@@ -45,7 +46,8 @@ pivot_df_Freedom = df_filtered_Freedom.pivot(
 
 sns.set_style("whitegrid")
 fig = plt.figure(figsize=(12, 12))
-fig.set_facecolor('#F9E79F')
+plt.subplots(2,2, figsize=(18,13))
+fig.set_facecolor('#fceee9')
 
 # Function to get color for country
 def get_country_color(country):
@@ -57,9 +59,18 @@ plt.text(0.7, 0.7, 'World Happiness Index 2015 - 2020', horizontalalignment='cen
          verticalalignment='center', fontsize=16, fontweight='bold')
 plt.axis('off')
 
+
+# Create a single legend and adjust its position using bbox_to_anchor
+handles = [plt.Line2D([0], [0], color=get_country_color(country), marker='o',
+                      markersize=12, linestyle='', label=country) for country in COUNTRY_NAMES]
+legend = plt.figlegend(handles=handles, labels=COUNTRY_NAMES, loc='center', ncol=5,
+                       fontsize=8, bbox_to_anchor=(0.5, 0.35), bbox_transform=plt.gcf().transFigure)
+
+
 # Second subplot: Happiness Index Bar Graph
 plt.subplot(3, 2, 3)
-pivot_df_Hindex.plot(kind='bar', width=0.8, ax=plt.gca(), color=[get_country_color(country) for country in pivot_df_Hindex.columns])
+pivot_df_Hindex.plot(kind='bar', width=0.8, ax=plt.gca(), color=[
+                     get_country_color(country) for country in pivot_df_Hindex.columns])
 plt.xlabel('Year', fontsize=12)
 plt.ylabel('Happiness Index', fontsize=12)
 plt.title('Happiness Index by Country for 2015 - 2020', fontsize=14)
@@ -67,7 +78,8 @@ plt.legend().remove()  # Remove individual legend
 
 # Third subplot: CPI Score Horizontal Bar Graph
 plt.subplot(3, 2, 4)
-pivot_df_CPI.plot(kind="barh", ax=plt.gca(), color=[get_country_color(country) for country in pivot_df_CPI.columns])
+pivot_df_CPI.plot(kind="barh", ax=plt.gca(), color=[
+                  get_country_color(country) for country in pivot_df_CPI.columns])
 plt.xlabel('CPI Score', fontsize=12)
 plt.ylabel('Year', fontsize=12)
 plt.title('CPI Score and Happiness Index', fontsize=14)
@@ -81,30 +93,33 @@ explode = [0.1 if i == 0 else 0 for i in range(
 plt.pie(
     df_filtered_dystopia["dystopia_residual"],
     labels=df_filtered_dystopia["Country"],
-    autopct='%1.1f%%',
+    # autopct='%1.1f%%',
     startangle=140,
-    colors=[get_country_color(country) for country in df_filtered_dystopia["Country"]],
+    colors=[get_country_color(country)
+            for country in df_filtered_dystopia["Country"]],
     explode=explode,
     shadow=True,
+    wedgeprops={'width': 0.4},  # Set the width to create a donut chart
 )
-plt.title('Dystopia Residual for 2020', fontsize=14)
-plt.legend().remove()  # Remove individual legend
 
+plt.title('Dystopia Residual for 2020', fontsize=14)
+plt.legend().remove() # Remove individual legend
+ 
 # Fifth subplot: Freedom Line Plot
 plt.subplot(3, 2, 6)
 plt.title('Freedom and Happiness Index', fontsize=14)
-pivot_df_Freedom.plot(kind="line", ax=plt.gca(), color=[get_country_color(country) for country in pivot_df_Freedom.columns])
+pivot_df_Freedom.plot(kind="line", ax=plt.gca(), color=[
+                      get_country_color(country) for country in pivot_df_Freedom.columns])
 plt.legend().remove()  # Remove individual legend
 
-# Create a single legend and adjust its position using bbox_to_anchor
-handles = [plt.Line2D([0], [0], color=get_country_color(country), marker='o', markersize=5, linestyle='', label=country) for country in COUNTRY_NAMES]
-legend = plt.figlegend(handles=handles, labels=COUNTRY_NAMES, loc='center', ncol=5, fontsize=8, bbox_to_anchor=(0.5, 0.35), bbox_transform=plt.gcf().transFigure)
+
 
 # Adjust layout and save the figure
 plt.tight_layout(pad=4.0, h_pad=6.0, w_pad=2.0)
 plt.subplots_adjust(wspace=0.8, hspace=0.6)
 
 # Save the plot
-plt.savefig('combined_plots.png', dpi=300, bbox_extra_artists=(legend,), bbox_inches='tight')
+plt.savefig('combined_plots.png', dpi=300,
+            bbox_extra_artists=(legend,), bbox_inches='tight')
 # Show the plots
 plt.show()
